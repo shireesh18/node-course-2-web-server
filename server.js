bfe02b5925342094f064eb73704dc095bd63aa34
Shireesh Kiran Marla <shireesh.kiran.marla@ibm.com@shireeshs-mbp.mynet>
2019-01-05 21:14:20 +0000

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const handlebars = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -62,6 +64,6 @@ app.get('/bad', (req, res) => {
     code : '500'
   });
 });
-app.listen(3000, ()=>{
-  console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
 });
